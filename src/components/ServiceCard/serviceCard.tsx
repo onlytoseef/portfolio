e94c053cf7d4b381e4710e192feb4c11fa79e61a
Toseef Rana <hellotoseef@gmail.com>
2024-05-21 21:41:02 +0500

    removed props error

--- src/components/ServiceCard/serviceCard.tsx
+++ src/components/ServiceCard/serviceCard.tsx
@@ -2,16 +2,10 @@ import React from 'react';
 
 interface ServiceCardProps {
   title: string;
-  price: string;
   imgSrc: string;
-  description: string;
 }
 
-const ServiceCard: React.FC<ServiceCardProps> = ({
-  title,
-  imgSrc,
-  description,
-}) => {
+const ServiceCard: React.FC<ServiceCardProps> = ({ title, imgSrc }) => {
   return (
     <div className='m-2 group px-10 py-5 service-card bg-white/10 rounded-lg flex flex-col items-center justify-center gap-2 relative after:absolute after:h-full after:bg-[#abd373]  z-20 shadow-lg after:-z-20 after:w-full after:inset-0 after:rounded-lg transition-all duration-300 hover:transition-all hover:duration-300 after:transition-all after:duration-500 after:hover:transition-all after:hover:duration-500 overflow-hidden cursor-pointer after:-translate-y-full after:hover:translate-y-0 [&_p]:delay-200 [&_p]:transition-all'>
       <img
@@ -22,9 +16,6 @@ const ServiceCard: React.FC<ServiceCardProps> = ({
       />
       <p className='cardtxt font-semibold text-gray-200 text-[1.5rem] tracking-wider group-hover:text-gray-700 text-xl'>
         {title}
-      </p>
-      <p className='blueberry font-semibold text-gray-600 text-xs'>
-        {description}
       </p>
     </div>
   );
